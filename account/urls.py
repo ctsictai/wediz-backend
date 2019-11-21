179c51c9b88a8de4ccf2b79110e29688dfa9abe4
ctsictai <ctsictai@gmail.com>
2019-11-21 21:07:43 +0900

    maker 기능 추가
    1. Maker model 생성(User model과 1:1 관계)
    2. CreateMaker View 엔드포인트 생성
    3. CreateMaker Unit test 구현
    4. fund unit test에서 User 변경사항 SocialPlatform 추가

--- account/urls.py
+++ account/urls.py
@@ -6,12 +6,13 @@ from .views      import (
                         ModifiedUserPhoto,
                         MakerCreate,
                         KakaoSigninView,
+                        MakerCreate
                         )
 urlpatterns = [
-	path('/signup', SignupView.as_view()),
-	path('/signin', SigninView.as_view()),
-	path('/modifyprofilephoto', ModifiedUserPhoto.as_view()),
-	path('/modifyprofile', ModifiedUserInfo.as_view()),
+    path('/signup', SignupView.as_view()),
+    path('/signin', SigninView.as_view()),
+    path('/modifyprofilephoto', ModifiedUserPhoto.as_view()),
+    path('/modifyprofile', ModifiedUserInfo.as_view()),
     path('/maker', MakerCreate.as_view()),
     path('/kakaosignin', KakaoSigninView.as_view())
 ]
